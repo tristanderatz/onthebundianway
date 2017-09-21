d481b14651b11372a38f3e400e62167a645775e7
tristanderatz <tristanderatz@users.noreply.github.com>
2017-09-21 18:18:33 +1000

    counter.js

---
+++ counter.js
@@ -0,0 +1,12 @@
+var start = 7500000;
+var speed = 1;
+$(document).ready(function () {
+    go();
+    setInterval(function () {
+        go();
+    }, speed);
+});
+function go() {
+    $("#counter").html(start.toFixed(0));
+    start += 0.125;
+}
